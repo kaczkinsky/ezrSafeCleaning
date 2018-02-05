a0bf5d864d1a11de78477f4d2486fd0948f38312
kaczkinsky <pierre.baconnier@bormuxhome.org>
2018-02-05 01:13:06 +0100

    Add a redirection from STDERR to a file and STDOUT

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ezrSafeCleaning
 A safe eraser of hard drive and/or files using Gutmann algorithm
+
+Decomments lines inquire in script to precise targets options.
--- ezrSafeCleaning.sh
+++ ezrSafeCleaning.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-######################  *******************
-# erzSafeCleaning.sh #  * FROM kaczkinski * :) #LiveInSafety
-######################  *******************
+######################   ***********************
+# ezrSafeCleaning.sh #  * MAINTAINER kaczkinsky *  #LiveInSafety ;)
+######################   ***********************
 
 if [ $USER != "root" ]
 then
@@ -10,11 +10,12 @@ then
   exit 1
 fi
 
-declare -a logfile="/var/log/erzSafeCleaning.log"
+declare -a logfile="/var/log/ezrSafeCleaning.log"
 
 # We try to know if a /dev/hda or /dev/sda is detected
 detect=$(ls -lh /dev/ | grep sda || grep hda)
-# $detect is available in verbose mode
+
+# Test if detect if true then print on STDOUT
 if [[ $detect ]]
 then
   echo Hard drive is detected and available to rewrite on
@@ -22,22 +23,26 @@ else
   echo Hard drive is not detected
 fi
 
-# We initialize a hard drive as a $var
-ddir[0]="/dev/sda"; ddir[1]="/dev/sdb"
-echo ${ddir[0]} ${ddir[1]}
+# We define the hard drive as $ddir
+#ddir[0]="/dev/sda*"; ddir[1]="/dev/hda*"
+ddir[0]="/home/kaczkinski/TEST/testfile"; ddir[1]="~/TEST/testfile"
+#echo ${ddir[0]} ${ddir[1]} # to use in debug
 
 # We ask which data we want to be delete
 read -p "Inquire partition will be deleting : " part
 part="$part"
-if [ "${ddir[0]}" == "$part" || "${ddir[1]}" == "$part" ]
+if [ "${ddir[0]}" == "$part" ] || [ "${ddir[1]}" == "$part" ]
 #if true
 then
-  read -p "Confirm rewriting on disk ? y or n : " val
+  read -p "Confirm rewriting on disk ? [y/n] : " val
   y="y"
   if [ "$val" == "$y" ]
   then
-    # Exec shred deletion and redirect logs of verbose mode then print on stdout
-    exec shred -v -n 70 --force --remove -z $part | tee -a $logfile # the redirection of shred cmd is not effective yet 
+    # Exec shred rewriting random numbers and erase /dev/sdX
+    # Do a redirection from STDERR to STDOUT then the output of shred -v 
+    # is duplicate to /var/log/ezrSafeCleaning.log
+    shred -vfz --random-source=/dev/urandom -n 70 -u $part 2>&1 | tee -a $logfile
+    # // X[$part] O[file_which_contains_absolute_filenames]
     echo "EXIT program"
     echo status ':' SUCCESS
   else
